bf2c5e76b759b7ad5c44461bf90d148fb82e8bfa
YouDontNeedToKnow <161518051+Ashnar2602@users.noreply.github.com>
2025-04-16 10:14:18 +0000

    Aggiornamento build e compressione in zip

--- obj/Release/net8.0/win-x64/CS_RPG_MK1.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/CS_RPG_MK1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_RPG_MK1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a56941c138e99871d3708b7a1268333a88711e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6811a1d3e37bc2281bd9c1ede0ffc9f18bdad43e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_RPG_MK1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_RPG_MK1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
